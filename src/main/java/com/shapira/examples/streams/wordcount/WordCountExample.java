768b90151bd87c54812c9052913e03bc50b46085
liufei <liufei32@outlook.com>
2024-07-31 18:39:49 +0800

    代码阅读注释

--- src/main/java/com/shapira/examples/streams/wordcount/WordCountExample.java
+++ src/main/java/com/shapira/examples/streams/wordcount/WordCountExample.java
@@ -18,8 +18,16 @@ public class WordCountExample {
     public static void main(String[] args) throws Exception{
 
         Properties props = new Properties();
+        /*
+         * 每个 Streams 应用程序都必须有一个应用程序 ID。这个 ID 即可用于协调应用程序实例，也可用于命名 内部的本地存储和相关主题。
+         * 对于同一个 Kafka 集群中的每一个 Streams 应用程序，这个 ID 必须是唯一 的。
+         */
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        /*
+         * default.value.serde 是 Kafka Streams 应用程序中的一个配置项，用于指定默认的值序列化/反序列化器（serde，serializer/deserializer的简称）。
+         * 当你创建 Kafka Streams 应用程序时，如果没有为某个特定的流或表显式地指定序列化/反序列化器，那么会使用 default.value.serde 中配置的序列化/反序列化器。
+         */
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
@@ -34,9 +42,8 @@ public class WordCountExample {
         // props.put(CommonClientConfigs.METADATA_MAX_AGE_CONFIG, 500);
 
         StreamsBuilder builder = new StreamsBuilder();
-
+        // 从 topic：wordcount-input中读取数据
         KStream<String, String> source = builder.stream("wordcount-input");
-
 
         final Pattern pattern = Pattern.compile("\\W+");
         KStream counts  = source.flatMapValues(value-> Arrays.asList(pattern.split(value.toLowerCase())))
@@ -44,6 +51,8 @@ public class WordCountExample {
                 .filter((key, value) -> (!value.equals("the")))
                 .groupByKey()
                 .count().mapValues(value->Long.toString(value)).toStream();
+
+        // 将结果输出到 topic：wordcount-output
         counts.to("wordcount-output");
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
